5dff6a38b524770062e6f71cdb6209535767beca
nicolettethiro <nicolette.thiro@cooper.edu>
2023-03-04 23:39:06 -0500

    fixed hardcoded pathname

--- Connect/db/Connect.java
+++ Connect/db/Connect.java
@@ -3,37 +3,36 @@ import java.sql.*;
 
 public class Connect {  
     /** 
-    * Connect to a sample database 
+     * Connect to a database 
+     * @param dbPath the path to the database file
      * @throws ClassNotFoundException
-    */  
-   public static void connect(String db_path) throws ClassNotFoundException {  
-       Connection conn = null;  
-       try {  
-           Class.forName("org.sqlite.JDBC");
-           // db parameters  
-           String url = "jdbc:sqlite:" + db_path;  
-           // create a connection to the database  
-           conn = DriverManager.getConnection(url);  
-             
-           System.out.println("Connection to SQLite has been established.");  
-             
-       } catch (SQLException e) {  
-           System.out.println(e.getMessage());  
-       } finally {  
-           try {  
-               if (conn != null) {  
-                   conn.close();  
-               }  
-           } catch (SQLException ex) {  
-               System.out.println(ex.getMessage());  
-           }  
-       }  
-   }  
-   /** 
-    * @param args the command line arguments 
- * @throws ClassNotFoundException
-    */  
-   public static void main(String[] args) throws ClassNotFoundException {  
-       connect("/Users/nishatahmed/Documents/GitHub/Game-Logic/NounBankSQlite.db");  
-   }  
+     */  
+    public static void connect(String dbPath) throws ClassNotFoundException {  
+        Connection conn = null;  
+        try {  
+            Class.forName("org.sqlite.JDBC");
+            // db parameters  
+            String url = "jdbc:sqlite:" + dbPath;  
+            // create a connection to the database  
+            conn = DriverManager.getConnection(url);  
+            System.out.println("Connection to SQLite has been established.");  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        } finally {  
+            try {  
+                if (conn != null) {  
+                    conn.close();  
+                }  
+            } catch (SQLException ex) {  
+                System.out.println(ex.getMessage());  
+            }  
+        }  
+    }  
+    /** 
+     * @param args the command line arguments 
+     * @throws ClassNotFoundException
+     */  
+    public static void main(String[] args) throws ClassNotFoundException {  
+        connect("NounBankSQlite.db");  
+    }  
 }  
