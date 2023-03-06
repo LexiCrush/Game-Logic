1f1a7aa3e4a3931df0ea2656fba8ccedd07a4ece
Armaan Kapoor <armaan.kapoor@cooper.edu>
2023-03-06 01:46:23 -0500

    Revert "made scoreboard and dealt with empty username/password for login"

--- Connect/UserDatabase.java
+++ Connect/UserDatabase.java
@@ -2,10 +2,10 @@ package Connect;
 
 import java.sql.*;
 
-public class CreateDatabase {
+public class UserDatabase {
 
     //Connect to sample database
-    public static void createNewDatabase(String dbPath, String sql) throws SQLException {
+    public static void createNewDatabase(String dbPath) throws SQLException {
 
         String url = "jdbc:sqlite:" + dbPath;
         Connection conn = DriverManager.getConnection(url);
@@ -17,7 +17,7 @@ public class CreateDatabase {
                 System.out.println("A new database has been created.");
                 try (Statement stmt = conn.createStatement()) {
                     stmt.execute("DROP TABLE IF EXISTS users;");
-                    stmt.execute(sql);
+                    stmt.execute("CREATE TABLE users (uid integer not null primary key, username varchar(50) not null unique, user_password varchar(255) not null);");
                     System.out.println("Successfully created table");
                 } catch (SQLException e) {
                     System.out.println("Couldn't create table " + e.getMessage());
@@ -32,10 +32,7 @@ public class CreateDatabase {
 
     //create user info database
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        String sql_user = "CREATE TABLE users (uid integer not null primary key, username varchar(50) not null unique, user_password varchar(255) not null);";
-        createNewDatabase("db/UserProfile.db", sql_user);
-        String sql_score = "CREATE TABLE scores (player_id integer not null primary key, uid integer not null, round_id integer not null, score integer not null, FOREIGN KEY (uid) REFERENCES users(uid));";
-        createNewDatabase("db/ScoreBoard.db" , sql_score);
+        createNewDatabase("db/UserProfile.db");
     }
 }
 
